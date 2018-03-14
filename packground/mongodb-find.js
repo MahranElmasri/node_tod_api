d87810f43565ee05f64110a076fccad0c842b5bf
“Mahran <Mahran_msri@yahoo.com>
2018-03-14 18:48:57 +0100

    use count method

--- packground/mongodb-find.js
+++ packground/mongodb-find.js
@@ -1,15 +1,23 @@
-const {MongoClient}=require('mongodb');
+const {MongoClient, ObjectID}=require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/Test',(err,db)=>{
     if(err){
         return console.log('Unable to connect MongoDB server',err);
     }
     console.log('conected tom MongoDB server');
-    db.collection('Users').find({name:'Mahran'}).toArray().then((docs)=>{
+    db.collection('Users').find({_id:new ObjectID('5aa954c079afd221ab2d82bc')}).toArray().then((docs)=>{
         console.log(JSON.stringify(docs,undefined,2));
     },(err)=>{
         if(err){
             return console.log('Unable to find record',err)
         }
+    });
+    db.collection('Users').find({name:'Mahran'}).count().then((count)=>{
+        console.log(`The records count is ${count}`)
+    },(err)=>{
+        if(err){
+            console.log('Unable to count the records ',err)
+        }
     })
+    db.close()
 })
