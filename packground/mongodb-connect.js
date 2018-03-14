164bb3fb460673ddd6923c3082c7c8d4f94e55ac
“Mahran <Mahran_msri@yahoo.com>
2018-03-14 15:16:12 +0100

    first commit

---
+++ packground/mongodb-connect.js
@@ -0,0 +1,49 @@
+// const MongoCleint=require('mongodb').MongoClient;
+
+// MongoCleint.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+//     if(err){
+//         return console.log('Unable to connect MongoDB server');
+    
+//     }
+//         console.log('connected to MongoDB server ');
+//         // db.collection('Todos').insertOne({
+//         //     text:'Something to do',
+//         //     completed:false
+//         // },(err,res)=>{
+//         //     if(err){
+//         //         return console.log('Unable to insert todo',err);
+//         //     }
+//         //     console.log(JSON.stringify(res.ops, undefined ,2));
+//         // })
+
+//         db.collection('Users').insertOne({
+//             name:'Mahran',
+//             age:37,
+//             location:'Berlin'
+//         },(err,res)=>{
+//             if(err){
+//                 return console.log('Unable to insert new user',err);
+//             }
+//             console.log(JSON.stringify(res.ops,undefined,2));
+//         })
+//     db.close();
+
+// })
+
+const MongoClient=require('mongodb').MongoClient;
+MongoClient.connect('mongodb://localhost:27017/Test',(err,db)=>{
+    if(err){
+        return console.log('Unable to connect MongoDB server');
+    }
+        console.log('connected to MongoDB server');
+        db.collection('Users').insertOne({
+            name:'Mahran',
+            age:33
+        },(err,res)=>{
+            if(err){
+                return console.log('Unable to insert new user',err);
+            }
+                console.log(res.ops);
+        })
+
+})
