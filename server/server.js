c351dd8d55f3cadab1db1229c78c3078ccde8959
“Mahran <Mahran_msri@yahoo.com>
2018-03-16 21:13:00 +0100

    add post test

--- server/server.js
+++ server/server.js
@@ -20,21 +20,66 @@
 // });
 
 // newTodo.save();
- const mongoose=require('mongoose');
+//  const mongoose=require('mongoose');
 
- mongoose.connect('mongodb://localhost:27017/Dci');
+//  mongoose.connect('mongodb://localhost:27017/Dci');
 
- var User=mongoose.model('User',{
-     name:{
-         type:String
-     },
-     address:{
-         type:String
-     }
- });
+//  var User=mongoose.model('User',{
+//      name:{
+//          type:String,
+//          required:true,
+//          minlength:1,
+//          trim:true
+//      },
+//      address:{
+//          type:String
+//      }
+//  });
 
- var newUser= new User({
-     name:'Mahran',
-     address:'Berlin'
- });
- newUser.save();
+//  var newUser= new User({
+//      name:'Mahran',
+//      address:'Berlin'
+//  });
+//  newUser.save();
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+mongoose.Promise=global.Promise;
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    name: req.body.name
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+// app.post('/users',(req,res)=>{
+//     var user=User({
+//         email:req.body.email
+//     });
+//     user.save().then((doc)=>{
+//         res.send(doc);
+//     },(err)=>{
+//         res.status(400).send(e)
+//     })
+// })
+
+
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+  });
+module.exports = {app};
