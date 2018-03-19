1a2ee42dfd36c9f48a2090e22bf6bd474895ae64
“Mahran <Mahran_msri@yahoo.com>
2018-03-19 21:59:03 +0100

    Hash user password before saving

--- packground/hashing.js
+++ packground/hashing.js
@@ -1,16 +1,33 @@
 var {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
-var message='Hi I am here';
-var hash=SHA256(message).toString();
+var bcrypt = require('bcryptjs');
 
-console.log(message);
-console.log(hash)
-var data={
-    id:10
-}
-var token=jwt.sign(data,'1234')
-console.log(token)
+var password='1234abc';
+//  bcrypt.genSalt(10, (err, salt) => {
+//       bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//       });
+//     });
+bcrypt.genSalt(10, (err, salt)=>{
+    bcrypt.hash(password, salt, (err, hash)=>{
+        console.log(hash)
+    })
+})
+var hashedPassword='$2a$10$MkJOS5U2oIAzqhPNjoTok.BZWHp/o90SUKUHEEwmFDZO7lk/sLE1y';
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+    console.log(res)
+})
+// var message='Hi I am here';
+// var hash=SHA256(message).toString();
 
-var decode=jwt.verify(token,'1234');
+// console.log(message);
+// console.log(hash)
+// var data={
+//     id:10
+// }
+// var token=jwt.sign(data,'1234')
+// console.log(token)
 
-console.log(decode)
+// var decode=jwt.verify(token,'1234');
+
+// console.log(decode)
